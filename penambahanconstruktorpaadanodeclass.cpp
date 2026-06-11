e7c587848b99bb977d746acd7b1cae3c38f95d67
virgygasya-lgtm <virgy.gasya@gmail.com>
2026-06-11 15:17:51 +0700

    penambahanconstruktorpaadanodeclass.cpp

---
+++ penambahanconstruktorpaadanodeclass.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+using namespace std;
+ 
+class Node
+{
+    public:
+    string data;
+    Node* leftchild;
+    Node* rightchild;
+
+    // Construktor for the node class
+    Node(string i, Node *l, Node *r)
+    {
+        data = i;
+        leftchild = l;
+        rightchild = r;
+    }
+};
+
